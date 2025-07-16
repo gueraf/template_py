6e9d0ed72d125a2f11cd3fe04453e73151333e5b
fabian <fabian.guera@gmail.com>
2025-07-16 12:42:07 +0400

    Use bazelisk if available.

--- update_requirements.sh
+++ update_requirements.sh
@@ -1,12 +1,23 @@
+#!/bin/bash
+
+# Check if bazelisk is available, otherwise use bazel
+if command -v bazelisk &> /dev/null; then
+    BAZEL_CMD="bazelisk"
+    echo "Using bazelisk"
+else
+    BAZEL_CMD="bazel"
+    echo "Using bazel"
+fi
+
 echo "Updating requirements_lock.txt" && \
-bazel run //:requirements && \
+$BAZEL_CMD run //:requirements && \
 echo "Updating pypi_pkgs_lock.bzl" && \
-bazel query 'kind("alias rule", deps(@pip//...))' --output=label_kind | \
+$BAZEL_CMD query 'kind("alias rule", deps(@pip//...))' --output=label_kind | \
   grep ":pkg" | \
   grep -oP "@.+" | \
   sed -e "s/.*/'&',/" -e '1s/^/PIPY_PKGS = [/' -e '$s/,$/]/' > pypi_pkgs_lock.bzl && \
 echo "Updating gazelle_python.yaml" && \
-bazel run //:gazelle_python_manifest.update && \
+$BAZEL_CMD run //:gazelle_python_manifest.update && \
 echo "Updating vscode settings.json" && \
-bazel build //.vscode:all && \
+$BAZEL_CMD build //.vscode:all && \
 ./bazel-bin/.vscode/vscode_import_generator
